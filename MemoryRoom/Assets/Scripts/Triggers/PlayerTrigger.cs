6ed66017a70adce5bf815d7dd39101e6d92d7e54
Antonis Varvaris <103698773+Antonisvar@users.noreply.github.com>
2024-06-04 15:50:38 +0300

    Saved the counters of the triggers
    The counter of the triggers will now be saved on a folder

--- MemoryRoom/Assets/Scripts/Triggers/PlayerTrigger.cs
+++ MemoryRoom/Assets/Scripts/Triggers/PlayerTrigger.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class PlayerTrigger : MonoBehaviour
 {
     public CustomTrigger kitchenTrigger;
     public CustomTrigger diningTrigger;
 
-
+    private int kitchenCount = 0;
+    private int diningCount = 0;
+    private string kitchenCountFilePath;
+    private string diningCountFilePath;
     void Awake()
     {
         kitchenTrigger.EnteredTrigger += OnKitchenTriggerEnter;
         diningTrigger.EnteredTrigger += OnDiningTriggerEnter;
+
+        kitchenCountFilePath = Path.Combine(Application.persistentDataPath, "kitchenCount.txt");
+        diningCountFilePath = Path.Combine(Application.persistentDataPath, "diningCount.txt");
+
     }
 
-    void OnTriggerEnter(Collider collider)
-    {
-        Debug.Log("Player entered trigger - says " + collider.name);
-    }
     void OnKitchenTriggerEnter(Collider collider)
     {
         Debug.Log("Player entered kitchen trigger - says " + collider.name);
+        kitchenCount++;
     }
 
     void OnDiningTriggerEnter(Collider collider)
     {
         Debug.Log("Player entered dining trigger - says" + collider.name);
+        diningCount++;
+    }
+
+    void SaveCounter(string filePath, int count)
+    {
+        try
+        {
+            File.AppendAllText(filePath, count.ToString() + Environment.NewLine);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to save counter to file: " + e.Message);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveCounter(kitchenCountFilePath, kitchenCount);
+        SaveCounter(diningCountFilePath, diningCount);
     }
 }
